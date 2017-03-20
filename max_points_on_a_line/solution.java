b8dd4e437587840078ff5383392e9371699f623a
sunpeng <sunpeng1996@aliyun.com>
2017-03-20 15:22:47 +0800

    test

---
+++ max_points_on_a_line/solution.java
@@ -0,0 +1,22 @@
+package max_points_on_a_line;
+
+/**
+ * Created by Apple on 2017/3/20.
+ *
+ * Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+ *
+ */
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution {
+    public int maxPoints(Point[] points) {
+
+    }
+}
